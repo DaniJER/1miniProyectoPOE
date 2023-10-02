e900adc1c8c1e65241ffb5e12a6f1e1cbac2df39
Jv-2011 <juannoviembre20@gmail.com>
2023-10-02 01:20:29 -0500

    Creación de la ventana de instrucciones

--- src/Ventana/ConfigGame.java
+++ src/Ventana/ConfigGame.java
@@ -1,7 +1,36 @@
 package Ventana;
 
-public class ConfigGame {
-    public void showInstructions(){
-        //Mostrar las intrucciones cuando se active el boton
+import javax.swing.*;
+import java.awt.*;
+
+public class ConfigGame extends JFrame {
+    public JPanel panel;
+    public ConfigGame(){
+        setSize(950,600); //Dimensiones de la ventana
+        setLocationRelativeTo(null); //Establecer la ubicacion de la ventana en el centro de la pantalla
+        setResizable(true); //Metodo para establecer si se redimensiona la ventana o no. el valor es booleano.
+        //setMinimumSize(new Dimension(300,600)); En caso de que se quiera establecer un tamaño minimo para la ventana
+        //setMaximumSize(new Dimension()); En caso de que se quiera establecer un tamaño maximo
+        setDefaultCloseOperation(EXIT_ON_CLOSE); //Se establece un Stop a la ventana, una vez se haga click en la X
+        setTitle("FIGURAS :)"); // Establecer titulo para la ventana
+        this.getContentPane() .setBackground(Color.gray); //Obtener el contenido de la ventana y establecer un color a su background
+        iniciarVentana();
+    }
+
+    private void iniciarVentana() {
+        panel = new JPanel();
+        panel.setLayout(null);
+        this.add(panel);
+    }
+
+    public JLabel showInstructions(){
+        JLabel texto = new JLabel();
+        texto.setOpaque(true);
+        texto.setText("Hola perras");
+        texto.setBounds(200,-220,950,600);
+        texto.setBackground(Color.white);
+        panel.add(texto);
+        return texto;
     }
 }
+
--- src/Ventana/Ventana.java
+++ src/Ventana/Ventana.java
@@ -14,7 +14,7 @@ public class Ventana extends JFrame { //La clase ventana hereda atributos de la 
         setResizable(true); //Metodo para establecer si se redimensiona la ventana o no. el valor es booleano.
         //setMinimumSize(new Dimension(300,600)); En caso de que se quiera establecer un tamaño minimo para la ventana
         //setMaximumSize(new Dimension()); En caso de que se quiera establecer un tamaño maximo
-        setDefaultCloseOperation(EXIT_ON_CLOSE); //Se establece un Stop a la ventana, una vez se clickee en la X
+        setDefaultCloseOperation(EXIT_ON_CLOSE); //Se establece un Stop a la ventana, una vez se haga click en la X
         setTitle("FIGURAS :)"); // Establecer titulo para la ventana
         this.getContentPane() .setBackground(Color.gray); //Obtener el contenido de la ventana y establecer un color a su background
         iniciarComponentes();
@@ -36,13 +36,27 @@ public class Ventana extends JFrame { //La clase ventana hereda atributos de la 
         botonPlay.setBackground(Color.black);
         panel.add(botonPlay);// Se agrega el boton "jugar al panel"botonPlay.addActionListener(new ActionListener()
         
-        //INTRUCCIONES
+        //INSTRUCCIONES
         JButton botonInstrucc = new JButton("Instrucciones");//Crear el boton de instrucciones
         botonInstrucc.setBounds(365,250,220,40); //ubicacion y tamano del boton "instrucciones"
         botonInstrucc.setForeground(Color.white); //Establecer color de la letra
         botonInstrucc.setFont(new Font("arial",0,30)); //Asignar
         botonInstrucc.setOpaque(true);
         botonInstrucc.setBackground(Color.black);
+
+        ActionListener capturarAccion = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                ConfigGame configuracion = new ConfigGame();
+                configuracion.setVisible(true);
+
+                ConfigGame instructions = new ConfigGame();
+                instructions.showInstructions();
+                instructions.setVisible(true);
+            }
+        };
+        botonInstrucc.addActionListener(capturarAccion);
         panel.add(botonInstrucc);// Se agrega el boton "Instrucciones" al panel
 
         //CREANDO LOS LABELS DE TIPO IMAGEN PARA LA VENTANA
