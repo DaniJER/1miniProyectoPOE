66382b3c265a3e7d8670d591febb214c024d1979
Daniel <danicraft.op7@gamil.com>
2023-10-03 12:08:04 -0500

    Cambios en el archivo logicaJuego

--- src/Ventana/Ventana.java
+++ src/Ventana/Ventana.java
@@ -48,7 +48,6 @@ public class Ventana extends JFrame { //La clase ventana hereda atributos de la 
                 juego.buttonAcept();
                 juego.buttonCancel();
                 juego.getTextPlayer();
-                juego.text1();
                 juego.setVisible(true);
 
             }
