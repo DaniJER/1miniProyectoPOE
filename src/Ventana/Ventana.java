fb9915f658a115633be9d049c40e5168253e99a1
Jv-2011 <juannoviembre20@gmail.com>
2023-10-03 13:05:12 -0500

    Cree la funcionalidad del boton volver

--- src/Ventana/Ventana.java
+++ src/Ventana/Ventana.java
@@ -53,10 +53,10 @@ public class Ventana extends JFrame { //La clase ventana hereda atributos de la 
             public void actionPerformed(ActionEvent e) {
                 dispose();
                 ConfigGame configuracion = new ConfigGame();
-                configuracion.setVisible(true);
                 configuracion.showInstructions();
                 configuracion.jugarButonn();
                 configuracion.volverButton();
+                configuracion.setVisible(true);
             }
         };
         botonInstrucc.addActionListener(capturarAccion);
